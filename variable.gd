9c4620718dc02e3e249935dce61d1d4d520ffa20
sajid-ahsan-seyam-26 <sajidahsanseyamstuden>
2026-05-08 00:47:24 +0600

    update

--- variable.gd
+++ variable.gd
@@ -11,4 +11,13 @@ func _process(delta: float) -> void:
  
 func _exit_tree() -> void:
     print("Node removed from scene tree")
-    
+
+
+cons Max_LEVEL :int=10
+var player_name:string="NOVA"
+var player_health:int=100
+var player_speed:=250.0
+var alive:bool=true
+func _ready()->void:
+    print("player name")
+    print("player health",health)
